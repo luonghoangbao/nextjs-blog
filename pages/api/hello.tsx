0ed83f394cacc1b3b68c8debf0f04d3703fb8f5d
hoangbao <baoluonghoang@gmail.com>
2020-07-24 14:15:27 +0700

    push json

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,4 +1,6 @@
-export default (req, res) => {
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json({ text: 'Hello user'})
 //     const email = req.body.email
 //   // Then save email to your database, etc...
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,7 +4,13 @@ import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({postData}) {
+export default function Post({postData} : {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -21,13 +27,31 @@ export default function Post({postData}) {
     )
 }
 export async function getStaticPaths() {
+    // // Call an external API endpoint to get posts
+    // const res = await fetch('https://.../posts')
+    // const posts = await res.json()
+  
+    // // Get the paths we want to pre-render based on posts
+    // const paths = posts.map((post) => `/posts/${post.id}`)
     const paths = getAllPostIds()
     return {
       paths,
       fallback: false
     }
   }
+  // This function gets called at build time
   export async function getStaticProps({ params }) {
+  //     // Call an external API endpoint to get posts
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
+
+  // // By returning { props: posts }, the Blog component
+  // // will receive `posts` as a prop at build time
+  // return {
+  //   props: {
+  //     posts,
+  //   },
+  // }
     const postData = await getPostData(params.id)
     return {
       props: {
